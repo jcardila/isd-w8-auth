d455fa1825c3f3ed253aa4a1d442fea98f6b1bf4
jcardila <andrea.rueda@kellogg.northwestern.edu>
2022-02-25 23:32:21 +0000

    basic authentication

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+    def new
+
+    end
+
+    def create
+        # find the user trying to login
+        @user = User.where({ email: params["email"] })[0]
+        if @user 
+            if @user.password == params["password"]
+                flash[:notice] = "You logged in!"
+                redirect_to "/companies"
+            else
+                flash[:notice] = "Error logging in!"
+                redirect_to "/sessions/new"
+            end
+        else
+            redirect_to "/sessions/new"
+        end
+    end
+end
